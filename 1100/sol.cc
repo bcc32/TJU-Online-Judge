db1a36d00ed2dab842ea9a410d84ca335c352858
bcc32 <bcc32@anapnea.net>
2011-12-31 23:02:38 -0500

    solve 1100

---
+++ 1100/sol.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a < b)
+        return gcd(b, a);
+    if (a % b == 0)
+        return b;
+    return gcd(b, a % b);
+}
+
+int main(void)
+{
+    int n;
+    for (cin >> n; n != 0; cin >> n)
+    {
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v.at(i);
+        int x = 0, y = n * (n - 1) / 2;
+        for (int i = 0; i < v.size(); i++)
+            for (int j = i + 1; j < v.size(); j++)
+                if (gcd(v.at(i), v.at(j)) == 1)
+                    x++;
+        if (x == 0)
+            cout << "No estimate for this data set." << endl;
+        else
+        {
+            cout.setf(ios_base::fixed);
+            cout.precision(6);
+            cout << sqrt(6.0 * y / x) << endl;
+        }
+    }
+}
