773dd62957f2e2fc6bebecdcb4f0f45187c692ea
bcc32 <bcc32@anapnea.net>
2012-01-01 20:40:54 -0500

    attempt 1008 (TLE)

---
+++ 1008/sol.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string permute(string s, vector<int> key)
+{
+    string temp(s);
+    for (int i = 0; i < key.size(); i++)
+        temp[key.at(i) - 1] = s[i];
+    return temp;
+}
+
+int main(void)
+{
+    int n;
+    for (cin >> n; n != 0; cin >> n)
+    {
+        vector<int> key(n);
+        for (int i = 0; i < n; i++)
+            cin >> key.at(i);
+        int k;
+        for (cin >> k; k != 0; cin >> k)
+        {
+            cin.ignore(1, ' ');
+            string s;
+            getline(cin, s);
+            while (s.size() < n)
+                s += ' ';
+            // k %= n; // FIXME SOME OTHER REDUCTION
+            for (int i = 0; i < k; i++)
+                s = permute(s, key);
+            cout << s << endl;
+        }
+        cout << endl;
+    }
+}
