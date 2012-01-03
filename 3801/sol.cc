fd3fdc0fd2860a80da5fd7b394dab6462f9b2d23
bcc32 <bcc32@anapnea.net>
2012-01-02 19:16:50 -0500

    solve 3801

---
+++ 3801/sol.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<string> ld(vector<string> v)
+{
+    vector<string> f;
+    for (vector<string>::iterator it = v.begin(); it < v.end(); it++)
+        f.push_back(it->substr(1));
+    return f;
+}
+
+bool test(vector<string> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v.at(i).size() == 0)
+            return true;
+        else if (count(v.begin(), v.end(), v.at(i)) > 1)
+            return true;
+    }
+    return false;
+}
+
+int main(void)
+{
+    int n;
+    for (cin >> n; n != 0; cin >> n)
+    {
+        vector<string> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v.at(i);
+        int c;
+        for (c = 0; !test(v); c++)
+            v = ld(v);
+        cout << c - 1 << endl;
+    }
+}
