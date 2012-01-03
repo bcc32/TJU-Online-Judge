5a4f00e5c8568ff54dfd83081c9e112280db11d6
bcc32 <bcc32@anapnea.net>
2012-01-03 17:50:02 -0500

    solve 1112

---
+++ 1112/sol.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    vector<string> v;
+    string s;
+    int max = 0;
+    for (cin >> s; s != "0"; cin >> s)
+    {
+        if (s.size() > max)
+            max = s.size();
+        reverse(s.begin(), s.end());
+        v.push_back(s);
+    }
+    string sum;
+    int carry = 0;
+    for (int i = 0; i < max; i++)
+    {
+        int x = carry;
+        for (int j = 0; j < v.size(); j++)
+            if (i < v.at(j).size())
+                x += (int)v.at(j)[i] - (int)'0';
+        carry = x / 10;
+        sum += (char)(x % 10) + '0';
+    }
+    while (carry > 0)
+    {
+        sum += (char)(carry % 10) + '0';
+        carry /= 10;
+    }
+    reverse(sum.begin(), sum.end());
+    cout << sum << endl;
+}
