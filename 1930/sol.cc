e9a1862bc18c17e06a63ab865468a440689ef0c6
bcc32 <bcc32@anapnea.net>
2012-01-04 07:48:18 -0500

    solve 1930

---
+++ 1930/sol.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    for (cin >> n; n != -1; cin >> n)
+    {
+        vector<int> v;
+        v.push_back(n);
+        int m;
+        for (cin >> m; m != 0; cin >> m)
+            v.push_back(m);
+        int c = 0;
+        for (int i = 0; i < v.size(); i++)
+            if (v.at(i) % 2 == 0)
+                for (int j = 0; j < v.size(); j++)
+                    if (v.at(j) * 2 == v.at(i))
+                        c++;
+        cout << c << endl;
+    }
+}
