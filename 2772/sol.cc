52cd3861f8b0f65dba79b221b59f59dbe0d57f04
bcc32 <bcc32@anapnea.net>
2012-01-07 11:36:13 -0500

    solve 2772

---
+++ 2772/sol.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int sqr(int x);
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int ax, ay, bx, by, cx, cy, dx, dy;
+        cin >> ax >> ay >> cx >> cy >> bx >> by >> dx >> dy;
+        if (sqr(ax - dx) + sqr(ay - dy) == sqr(bx - cx) + sqr(by - cy) &&
+            sqr(ax - bx) + sqr(ay - by) == sqr(dx - cx) + sqr(dy - cy))
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+}
+
+int sqr(int x)
+{
+    return x * x;
+}
