d22610d2e84d8a7db9d6217887e994107fb3f9f0
bcc32 <bcc32@anapnea.net>
2012-01-07 10:50:14 -0500

    solve 2468

---
+++ 2468/sol.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        map<char, int> m;
+        string s;
+        cin >> s;
+        int max = 0;
+        for (string::iterator it = s.begin(); it < s.end(); it++)
+            m[*it]++;
+        for (char c = 'a'; c <= 'z'; c++)
+            if (m[c] > max)
+                max = m[c];
+        for (char c = 'a'; c <= 'z'; c++)
+            if (m[c] == max)
+                cout << c;
+        cout << endl;
+    }
+}
