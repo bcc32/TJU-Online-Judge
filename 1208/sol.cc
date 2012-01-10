c305023d7d48b22cc45d68506d5aaa59f68589ab
bcc32 <bcc32@anapnea.net>
2012-01-10 10:37:33 -0500

    solve 1208

---
+++ 1208/sol.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string add(string a, string b);
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    string a = "1", b = "1";
+    for (int i = 2; i < n; i++)
+    {
+        string t = add(a, b);
+        a = b;
+        b = t;
+    }
+    cout << b << endl;
+}
+
+string add(string a, string b)
+{
+    string s;
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    bool c = false;
+    for (int i = 0; i < a.size() || i < b.size(); i++)
+    {
+        int x = 0;
+        if (i < a.size())
+            x += (int)a[i] - (int)'0';
+        if (i < b.size())
+            x += (int)b[i] - (int)'0';
+        if (c)
+            x++;
+        if (x > 9)
+        {
+            c = true;
+            x -= 10;
+        }
+        else
+            c = false;
+        s += (char)x + '0';
+    }
+    if (c)
+        s += '1';
+    reverse(s.begin(), s.end());
+    return s;
+}
