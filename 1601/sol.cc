9ddeea112794be30620b66c1bf25ac2b506778fb
bcc32 <bcc32@anapnea.net>
2012-01-11 22:25:17 -0500

    solve 1601

---
+++ 1601/sol.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#define abs(x) ((x) > 0 ? (x) : -(x))
+using namespace std;
+
+int main(void)
+{
+    int n, i = 1;
+    for (cin >> n; n != 0; cin >> n, i++)
+    {
+        cout << "Set #" << i << endl;
+        vector<int> v(n);
+        int s = 0;
+        for (int j = 0; j < n; j++)
+        {
+            cin >> v[j];
+            s += v[j];
+        }
+        int a = s / n;
+        int x = 0;
+        for (vector<int>::iterator it = v.begin(); it < v.end(); it++)
+            x += abs(*it - a);
+        cout << "The minimum number of moves is " << x / 2 << "." << endl
+            << endl;
+    }
+}
