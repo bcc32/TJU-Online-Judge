65d61b5d4685fdbbfa05570687c90ee8589621f1
bcc32 <bcc32@anapnea.net>
2012-01-14 23:29:37 -0500

    attempt 3043 (TLE)
    use vector<bool> instead of map<int, bool>
    seriously, why did i use std::map?

--- 3043/sol.cc
+++ 3043/sol.cc
@@ -1,19 +1,22 @@
 #include <iostream>
-#include <map>
+#include <vector>
+#define min(x, y) ((x) < (y) ? (x) : (y))
+#define max(x, y) ((x) > (y) ? (x) : (y))
 using namespace std;
 
 bool primeq(int n);
 
 int main(void)
 {
-    map<int, bool> p;
+    vector<bool> p(1000000);
     for (int i = 0; i < 1000000; i++)
-        if (primeq(i)) p[i] = true;
-        else p[i] = false;
+        p[i] = primeq(i);
     int L, U;
     for (cin >> L >> U; L != -1 || U != -1; cin >> L >> U)
     {
         int c = 0, d = 0;
+        L = max(L, 0);
+        U = min(U, 999999);
         for (int i = L; i <= U; i++)
             if (p[i])
             {
