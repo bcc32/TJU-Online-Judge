edbfb43ccc7fb6a74ed3c6e295ea7eeb4ef3037c
bcc32 <bcc32@anapnea.net>
2012-01-16 17:47:55 -0500

    re-solve 3232
    the easy way... with math

--- 3232/sol.cc
+++ 3232/sol.cc
@@ -1,39 +1,10 @@
 #include <iostream>
-#include <vector>
+#define max(x, y) ((x) > (y) ? (x) : (y))
 using namespace std;
-
-bool validq(vector<int> v);
-void move(vector<int> &v);
 
 int main(void)
 {
-    int a, b, c, n;
+    int a, b, c;
     cin >> a >> b >> c;
-    vector<int> v;
-    v.push_back(a);
-    v.push_back(b);
-    v.push_back(c);
-    for (n = 0; validq(v); n++)
-        move(v);
-    cout << n << endl;
-}
-
-bool validq(vector<int> v)
-{
-    return v[2] - v[1] != 1 || v[1] - v[0] != 1;
-}
-
-void move(vector<int> &v)
-{
-    int t = v[1];
-    if (v[2] - v[1] > v[1] - v[0])
-    {
-        v[1] = v[2] - 1;
-        v[0] = t;
-    }
-    else
-    {
-        v[1] = v[0] + 1;
-        v[2] = t;
-    }
+    cout << max(c - b, b - a) - 1 << endl;
 }
