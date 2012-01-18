71aaa2f829a5a3ea19bd2935d4b3af9c91018733
bcc32 <bcc32@anapnea.net>
2012-01-18 15:38:28 -0500

    attempt 2196 (WA)

--- 2196/sol.cc
+++ 2196/sol.cc
@@ -2,28 +2,35 @@
 #include <set>
 using namespace std;
 
+bool cmp(string a, string b);
+
 int main(void)
 {
     int n;
     for (cin >> n; n != 0; cin >> n)
     {
-        set<long long> s;
+        set<string> s;
         for (int i = 0; i < n; i++)
         {
             char c;
             cin >> c;
             if (c == 'B')
             {
-                long long x;
+                string x;
                 cin >> x;
                 s.insert(x);
             }
             else if (c == 'G')
             {
-                set<long long>::iterator it = s.begin();
+                set<string>::iterator it = s.begin();
                 cout << *it << endl;
                 s.erase(it);
             }
         }
     }
+}
+
+bool cmp(string a, string b)
+{
+    return a.size() < b.size() || (a.size() <= b.size() && a < b);
 }
