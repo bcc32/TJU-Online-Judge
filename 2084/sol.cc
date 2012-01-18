b633db0ec4eed1dabd2cb8de1f83485e66223023
bcc32 <bcc32@anapnea.net>
2012-01-17 20:52:44 -0500

    solve 2084
    I forgot "i++"... :/

--- 2084/sol.cc
+++ 2084/sol.cc
@@ -10,7 +10,7 @@ int main(void)
     cout.precision(2);
     double d, t;
     int r, i = 1;
-    for (cin >> d >> r >> t; r != 0; cin >> d >> r >> t)
-        cout << "Trip #" << i << ": " << 2 * PI * (d / 2 / MIIN) * r << " "
-            << 2 * PI * (d / 2 / MIIN) * r / (t / HRSEC) << endl;
+    for (cin >> d >> r >> t; r != 0; cin >> d >> r >> t, i++)
+        cout << "Trip #" << i << ": " << PI * d * r / MIIN << " "
+            << PI * d * r * HRSEC / t / MIIN << endl;
 }
