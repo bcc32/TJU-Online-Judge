0dcdc81cdb920289191cf06588cde0aeb74b389b
bcc32 <bcc32@anapnea.net>
2012-01-17 19:54:26 -0500

    solve 2076

--- 2076/sol.cc
+++ 2076/sol.cc
@@ -15,9 +15,9 @@ int main(void)
         for (int j = 0; j < m; j++)
             v[j] = true;
         for (int j = 2; j <= m; j++)
-            for (int k = 1; k < m - 1; k++)
-                if (k % j == 0)
+            for (int k = 0; k < m; k++)
+                if ((k + 1) % j == 0)
                     v[k] = !v[k];
-        cout << count(v.begin(), v.end(), true) - 1 << endl;
+        cout << count(v.begin(), v.end(), true) << endl;
     }
 }
