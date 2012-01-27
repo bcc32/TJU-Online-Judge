bfbb858ee537f15bec481eb02e486c93da67bde6
bcc32 <bcc32@anapnea.net>
2012-01-27 15:05:25 -0500

    attempt 3259 (TLE)

--- 3259/sol.cc
+++ 3259/sol.cc
@@ -11,10 +11,11 @@ int main(void)
     for (cin >> a >> b; !cin.eof(); cin >> a >> b)
     {
         int c = 0;
-        if (b > v.size())
+        if (b >= v.size())
         {
+            int s = v.size();
             v.resize(b + 1);
-            for (int i = v.size() + 1; i < b; i++)
+            for (int i = s; i <= b; i++)
                 v[i] = nfactor(i);
         }
         for (int i = a; i <= b; i++)
