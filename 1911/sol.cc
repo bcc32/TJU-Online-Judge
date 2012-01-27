e6fff15dca6ee1c83080e0072ddc7ae5677cd0ea
bcc32 <bcc32@anapnea.net>
2012-01-26 20:31:21 -0500

    attempt 1911 (WA)

---
+++ 1911/sol.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#define vowelq(c) ((c) == 'a' || (c) == 'e' || (c) == 'i' || (c) == 'o' || \
+    (c) == 'u')
+using namespace std;
+
+bool t1(string s);
+bool t2(string s);
+bool t3(string s);
+
+int main(void)
+{
+    string s;
+    for (cin >> s; !cin.eof(); cin >> s)
+    {
+        if (t1(s) && t2(s) && t3(s))
+            cout << "<" << s << "> is acceptable." << endl;
+        else
+            cout << "<" << s << "> is not acceptable." << endl;
+    }
+}
+
+bool t1(string s)
+{
+    for (string::iterator it = s.begin(); it < s.end(); it++)
+        if (vowelq(*it))
+            return true;
+    return false;
+}
+
+bool t2(string s)
+{
+    bool b1, b2;
+    for (string::iterator it = s.begin() + 2; it < s.end(); it++)
+    {
+        if ((vowelq(*it) && b1 && b2) || (!vowelq(*it) && !b1 && !b2))
+            return false;
+        b1 = b2;
+        b2 = vowelq(*it);
+    }
+    return true;
+}
+
+bool t3(string s)
+{
+    char l;
+    for (string::iterator it = s.begin() + 1; it < s.end(); it++)
+    {
+        if (*it == l && l != 'e' && l != 'o') return false;
+        l = *it;
+    }
+    return true;
+}
