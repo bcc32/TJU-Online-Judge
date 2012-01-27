48c5b34524b7b41232c68b2233376842f67efe0f
bcc32 <bcc32@anapnea.net>
2012-01-27 18:52:26 -0500

    attempt 2437 (WA)

---
+++ 2437/sol.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    string s;
+    cin >> s;
+    for (bool f = true; f; ) // XXX ugly...
+    {
+        f = false;
+        for (string::iterator it = s.begin() + 2; it < s.end(); it++)
+            if (*it == *(it - 2))
+            {
+                s.erase(it);
+                s.erase(it - 2);
+                f = true;
+                break;
+            }
+    }
+    cout << s.size() << endl;
+}
