aafc98ab2b100047c62f217c42574deb37b83635
bcc32 <bcc32@anapnea.net>
2012-02-08 18:08:49 -0500

    revise implementation of 2762

--- 2762/sol.cc
+++ 2762/sol.cc
@@ -1,40 +1,20 @@
-#include <cstdio>
+#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
-int min(vector<int>::iterator f, vector<int>::iterator b);
-int max(vector<int>::iterator f, vector<int>::iterator b);
 
 int main(void)
 {
     int n, q;
-    scanf("%d%d", &n, &q);
+    cin >> n >> q;
     vector<int> v(n);
     for (int i = 0; i < n; i++)
-        scanf("%d", &v[i]);
+        cin >> v[i];
     for (int i = 0; i < q; i++)
     {
         int a, b;
-        scanf("%d%d", &a, &b);
-        printf("%d\n", max(v.begin() + a - 1, v.begin() + b) - 
-            min(v.begin() + a - 1, v.begin() + b));
+        cin >> a >> b;
+        cout << max_element(v.begin() + a - 1, v.begin() + b) -
+            min_element(v.begin() + a - 1, v.begin() + b) << endl;
     }
-}
-
-int min(vector<int>::iterator a, vector<int>::iterator b)
-{
-    int m = *a;
-    for (vector<int>::iterator it = a + 1; it < b; it++)
-        if (*it < m)
-            m = *it;
-    return m;
-}
-
-int max(vector<int>::iterator a, vector<int>::iterator b)
-{
-    int m = *a;
-    for (vector<int>::iterator it = a + 1; it < b; it++)
-        if (*it > m)
-            m = *it;
-    return m;
 }
