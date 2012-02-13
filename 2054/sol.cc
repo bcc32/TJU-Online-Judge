c95a856d505f31fb966f16c790b6379fab6714ce
bcc32 <bcc32@anapnea.net>
2012-02-12 22:32:34 -0500

    solve 2054
    using min_element and pairs

--- 2054/sol.cc
+++ 2054/sol.cc
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
 using namespace std;
+
+bool cmp(pair< pair<int, int>, pair<int, int> > a, pair< pair<int, int>, 
+    pair<int, int> > b);
 
 int main(void)
 {
@@ -8,39 +14,42 @@ int main(void)
     for (cin >> dollars >> foo >> cents >> a >> b >> c >> d; !cin.eof();
         cin >> dollars >> foo >> cents >> a >> b >> c >> d)
     {
-        int x = 100 * dollars + cents;
-        int n = 0, Q = 0, D = 0, N = 0, P = 0;
-        bool f = true;
-        while (n < x)
+        int n = 100 * dollars + cents;
+        vector< pair< pair<int, int>, pair<int, int> > > v;
+        for (int x = 0; x <= a; x++)
         {
-            if (a > 0 && n + 25 <= x)
+            if (25 * x > n) break;
+            for (int y = 0; y <= b; y++)
             {
-                n += 25;
-                a--; Q++;
-            }
-            else if (b > 0 && n + 10 <= x)
-            {
-                n += 10;
-                b--; D++;
-            }
-            else if (c > 0 && n + 5 <= x)
-            {
-                n += 5;
-                c--; N++;
-            }
-            else if (d > 0)
-            {
-                n++;
-                d--; P++;
-            }
-            else
-            {
-                cout << "NO EXACT CHANGE" << endl;
-                f = false;
-                break;
+                if (25 * x + 10 * y > n) break;
+                for (int z = 0; z <= c; z++)
+                {
+                    if (25 * x + 10 * y + 5 * z > n) break;
+                    for (int w = 0; w <= d; w++)
+                    {
+                        if (25 * x + 10 * y + 5 * z + w > n) break;
+                        else if (25 * x + 10 * y + 5 * z + w == n)
+                            v.push_back(make_pair(make_pair(x, y),
+                                make_pair(z, w)));
+                    }
+                }
             }
         }
-        if (f)
-            cout << Q << " " << D << " " << N << " " << P << endl;
+        if (v.size() > 0)
+        {
+            pair< pair<int, int>, pair<int, int> > p =
+                *min_element(v.begin(), v.end(), cmp);
+            cout << p.first.first << " " << p.first.second << " " << 
+                p.second.first << " " << p.second.second << endl;
+        }
+        else
+            cout << "NO EXACT CHANGE" << endl;
     }
+}
+
+bool cmp(pair< pair<int, int>, pair<int, int> > a, pair< pair<int, int>, 
+    pair<int, int> > b)
+{
+    return a.first.first + a.first.second + a.second.first + a.second.second < 
+        b.first.first + b.first.second + b.second.first + b.second.second;
 }
