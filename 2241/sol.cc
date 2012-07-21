a9fffdeb2ebae05b8f9052f423705e6671beecee
bcc32 <bcc32@blinkenshell.org>
2012-07-21 13:44:24 -0400

    attempt 2241 (MLE)

--- 2241/sol.cc
+++ 2241/sol.cc
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <deque>
-#include <map>
+#include <algorithm>
 using namespace std;
 
-map< int, vector<int> > t;
+vector< vector<int> > v;
 
-void make_link(int, int);
-bool find_path(int, int);
+void add_ancestors(int, int);
 
 int main(void)
 {
@@ -15,50 +13,33 @@ int main(void)
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        t.clear();
+        v.clear();
         int m;
         cin >> m;
+        v.resize(m);
         for (int j = 1; j < m; j++)
         {
             int a, b;
             cin >> a >> b;
-            make_link(a, b);
+            add_ancestors(a, b);
         }
         cin >> m;
         for (int j = 0; j < m; j++)
         {
             int a, b;
             cin >> a >> b;
-            cout << (find_path(a, b) ? "Yes" : "No") << endl;
+            if (find(v[b].begin(), v[b].end(), a) != v[b].end())
+                cout << "Yes" << endl;
+            else
+                cout << "No" << endl;
         }
     }
 }
 
-void make_link(int a, int b) // a is the child of b
+void add_ancestors(int a, int b)
 {
-    if (t.find(b) == t.end())
-        t[b] = vector<int>();
-    t[b].push_back(a);
-}
-
-bool find_path(int a, int b) // is b the child of a?
-{
-    deque<int> open_list;
-    open_list.push_back(a);
-    map<int, int> dist;
-    dist[a] = 0;
-    while (open_list.size() > 0)
-    {
-        int curr = open_list.front();
-        open_list.pop_front();
-        for (int i = 0; i < t[curr].size(); i++)
-            if (dist.find(t[curr][i]) == dist.end())
-            {
-                dist[t[curr][i]] = dist[curr] + 1;
-                open_list.push_back(t[curr][i]);
-            }
-        if (dist.find(b) != dist.end() && dist[b] > 0)
-            return true;
-    }
-    return false;
+    v[a].push_back(b);
+    if (b > 0)
+        for (int i = 0; i < v[b].size(); i++)
+            add_ancestors(a, v[b][i]);
 }
