60620e2506279a5af3f7debe7388421b0c5c5ca0
bcc32 <bcc32@blinkenshell.org>
2012-07-31 00:38:35 -0400

    solve 1457

--- 1457/sol.cc
+++ 1457/sol.cc
@@ -9,7 +9,7 @@ int main(void)
     {
         int s, d;
         cin >> s >> d;
-        if (s > d && (s - d) % 2 == 0)
+        if (s >= d && (s - d) % 2 == 0)
             cout << (s + d) / 2 << " " << (s - d) / 2 << endl;
         else
             cout << "impossible" << endl;
