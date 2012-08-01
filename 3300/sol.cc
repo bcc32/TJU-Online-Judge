ec9980982bab1dc3fe84f39dac256c8f7ebaeb3e
bcc32 <bcc32@blinkenshell.org>
2012-08-01 00:21:24 -0400

    attempt 3300 (TLE)

--- 3300/sol.cc
+++ 3300/sol.cc
@@ -3,27 +3,40 @@
 #include <map>
 using namespace std;
 
+map<int, int> m;
+
+int gcd(int, int);
 int phi(int);
 
 int main(void)
 {
-    map<int, int> m;
     int a, b;
     for (cin >> a >> b; !cin.eof(); cin >> a >> b)
     {
         int s = 0;
         for (int i = a; i <= b; i++)
-        {
-            if (m.find(i) == m.end())
-                m[i] = phi(i);
-            s += m[i];
-        }
+            s += phi(i);
         cout << s << endl;
     }
 }
 
+int gcd(int a, int b)
+{
+    if (a < b)
+        return gcd(b, a);
+    if (a % b == 0)
+        return b;
+    else
+        return gcd(b, a % b);
+}
+
 int phi(int n)
 {
+    if (m.find(n) != m.end())
+        return m[n];
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0 && gcd(i, n / i) == 1)
+            return phi(i) * phi(n / i);
     vector<int> pf;
     int foo = n;
     for (int i = 2; i * i <= n; i++)
