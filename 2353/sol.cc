fc471212375e9dd4d5fc6321b3dbac4b305c27e2
bcc32 <bcc32@blinkenshell.org>
2012-08-19 00:00:49 -0400

    solve 2353

--- 2353/sol.cc
+++ 2353/sol.cc
@@ -10,30 +10,37 @@ int main(void)
     {
         int m;
         cin >> m;
-        set<int> s;
+        set<int> s, d;
         for (int j = 0; j < m; j++)
         {
             int x;
             cin >> x;
             s.insert(x);
+            for (set<int>::iterator it = s.begin(); it != s.end(); ++it)
+                if (*it > x)
+                    for (int i = 1; i * i <= *it - x; i++)
+                    {
+                        if ((*it - x) % i == 0)
+                        {
+                            d.insert(i);
+                            d.insert((*it - x) / i);
+                        }
+                    }
+                else if (*it < x)
+                    for (int i = 1; i * i <= x - *it; i++)
+                    {
+                        if ((x - *it) % i == 0)
+                        {
+                            d.insert(i);
+                            d.insert((x - *it) / i);
+                        }
+                    }
         }
         for (int foo = 1; foo <= 1000000; foo++)
-        {
-            bool done = true;
-            set<int> bar;
-            for (set<int>::iterator it = s.begin(); it != s.end(); ++it)
-                if (bar.find(*it % foo) == bar.end())
-                    bar.insert(*it % foo);
-                else
-                {
-                    done = false;
-                    break;
-                }
-            if (done)
+            if (d.find(foo) == d.end())
             {
                 cout << foo << endl;
                 break;
             }
-        }
     }
 }
