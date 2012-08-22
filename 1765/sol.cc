2febfdb247c1c0fd6b778ee44c11b0250e8aa097
bcc32 <bcc32@blinkenshell.org>
2012-08-21 20:54:49 -0400

    attempt 1765 (WA)

---
+++ 1765/sol.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    vector<int> v(n), u(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    u[0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        int m = -1;
+        for (int j = 0; j < n; j++)
+            if (v[j] < v[i] && u[j] > u[m])
+                m = j;
+        if (m >= 0)
+            u[i] = u[m] + 1;
+        else
+            u[i] = u[i - 1];
+    }
+    cout << u[n - 1] << endl;
+}
