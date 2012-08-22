8c516235ac41ee208c879d7877a9a344b4c6a32e
bcc32 <bcc32@blinkenshell.org>
2012-08-21 20:31:21 -0400

    solve 2843

--- 2843/sol.cc
+++ 2843/sol.cc
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <vector>
+#define max(a, b) ((a) > (b) ? (a) : (b))
 using namespace std;
-
-int max(int a, int b);
-int find(vector< vector<int> >::iterator u, vector< vector<int> >::iterator v,
-    int i); // i: last used index
 
 int main(void)
 {
@@ -14,40 +11,23 @@ int main(void)
     {
         int m;
         cin >> m;
-        vector< vector<int> > v(m);
+        vector< vector<int> > v(m), u(m);
         for (int j = 0; j < m; j++)
         {
             v[j].resize(3);
             cin >> v[j][0] >> v[j][1] >> v[j][2];
         }
-        cout << find(v.begin(), v.end(), -1) << endl;
-    }
-}
-
-int max(int a, int b) // macro doesn't work because it repeats the calculation
-{
-    if (a > b) return a;
-    else return b;
-}
-
-int find(vector< vector<int> >::iterator u, vector< vector<int> >::iterator v,
-    int i)
-{
-    if (u == v)
-        return 0;
-    else
-    {
-        if (i == 0)
-            return max((*u)[1] + find(u + 1, v, 1),
-                (*u)[2] + find(u + 1, v, 2));
-        else if (i == 1)
-            return max((*u)[0] + find(u + 1, v, 0),
-                (*u)[2] + find(u + 1, v, 2));
-        else if (i == 2)
-            return max((*u)[0] + find(u + 1, v, 0),
-                (*u)[1] + find(u + 1, v, 1));
-        else
-            return max(max((*u)[0] + find(u + 1, v, 0), (*u)[1] +
-                find(u + 1, v, 1)), (*u)[2] + find(u + 1, v, 2));
+        u[0].resize(3);
+        u[0][0] = v[0][0];
+        u[0][1] = v[0][1];
+        u[0][2] = v[0][2];
+        for (int j = 1; j < m; j++)
+        {
+            u[j].resize(3);
+            u[j][0] = v[j][0] + max(u[j - 1][1], u[j - 1][2]);
+            u[j][1] = v[j][1] + max(u[j - 1][0], u[j - 1][2]);
+            u[j][2] = v[j][2] + max(u[j - 1][0], u[j - 1][1]);
+        }
+        cout << max(max(u[m - 1][0], u[m - 1][1]), u[m - 1][2]) << endl;
     }
 }
