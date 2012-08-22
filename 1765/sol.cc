d2368bc9cae1666063477a4fbd9539abc0f90d56
bcc32 <bcc32@blinkenshell.org>
2012-08-21 21:32:40 -0400

    attempt 1765 (WA)

--- 1765/sol.cc
+++ 1765/sol.cc
@@ -12,14 +12,18 @@ int main(void)
     u[0] = 1;
     for (int i = 1; i < n; i++)
     {
-        int m = -1;
-        for (int j = 0; j < n; j++)
+        int m = -1, M = -1;
+        for (int j = 0; j < i; j++)
+        {
             if (v[j] < v[i] && (m < 0 || u[j] > u[m]))
                 m = j;
+            if (u[j] > u[M])
+                M = j;
+        }
         if (m >= 0)
             u[i] = u[m] + 1;
         else
-            u[i] = u[i - 1];
+            u[i] = u[M];
     }
     cout << u[n - 1] << endl;
 }
