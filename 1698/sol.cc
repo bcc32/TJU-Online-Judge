33ca18e14ac340b2be4f4ce32595413e5637ff4a
Kuroneko <bcc32@blinkenshell.org>
2012-12-11 08:09:57 -0500

    revise 1689 (WA)

--- 1698/sol.cc
+++ 1698/sol.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#define next(i) ((i) == 9 ? 7 : (i) == 7 ? 3 : 9)
+#define next(i) ((i) == 9 ? 3 : (i) == 3 ? 7 : 3)
 using namespace std;
 
 int cksum(int a, int n, int b);
@@ -11,7 +11,7 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         cout << "Scenario #" << i + 1 << ":" << endl;
-        int a, b;
+        int a, b; // FIXME b can have leading zeroes
         cin >> a;
         cin.get(); // ignore '?'
         cin >> b;
@@ -19,23 +19,23 @@ int main(void)
         for (n = 0; n < 10; n++)
             if (cksum(a, n, b) % 10 == 0)
                 break;
-        cout << a << n << b << endl;
+        cout << a << n << b << endl << endl;
     }
 }
 
-int cksum(int a, int n, int b) // FIXME WRONG DIRECTION
+int cksum(int a, int n, int b)
 {
     int s = 0;
     int k;
-    for (k = 9; a > 0; k = next(k))
-    {
-        s += k * (a / 10);
-        a %= 10;
-    }
-    s += k * n;
     for (k = next(k); b > 0; k = next(k))
     {
-        s += k * (b / 10);
+        s += k * (b % 10);
         b /= 10;
+    }
+    s += k * n;
+    for (k = 9; a > 0; k = next(k))
+    {
+        s += k * (a % 10);
+        a /= 10;
     }
 }
