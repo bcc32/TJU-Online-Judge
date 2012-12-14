4eee54b7d0068d64076e4254260d8e363ab26d6b
Kuroneko <bcc32@blinkenshell.org>
2012-12-13 22:19:36 -0500

    attempt 1397 (WA)

--- 1397/sol.c
+++ 1397/sol.c
@@ -23,34 +23,51 @@ int main(void)
 
 int valid()
 {
-    int c = 0, x = 0, o = 0, i, j;
-    if (grid[0][0] == grid[1][0] && grid[0][0] == grid[2][0] &&
-            grid[0][0] != '.')
-        c++;
-    if (grid[0][1] == grid[1][1] && grid[0][1] == grid[2][1] &&
-            grid[0][1] != '.')
-        c++;
-    if (grid[0][2] == grid[1][2] && grid[0][2] == grid[2][2] &&
-            grid[0][2] != '.')
-        c++;
-    if (grid[0][0] == grid[0][1] && grid[0][0] == grid[0][2] &&
-            grid[0][0] != '.')
-        c++;
-    if (grid[1][0] == grid[1][1] && grid[1][0] == grid[1][2] &&
-            grid[1][0] != '.')
-        c++;
-    if (grid[2][0] == grid[2][1] && grid[2][0] == grid[2][2] &&
-            grid[2][0] != '.')
-        c++;
-    if (grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2] &&
-            grid[0][0] != '.')
-        c++;
-    if (grid[2][0] == grid[1][1] && grid[2][0] == grid[0][2] &&
-            grid[2][0] != '.')
-        c++;
-    if (c > 1)
+    int x = 0, o = 0, i, j;
+    if (grid[0][0] == grid[1][0] && grid[0][0] == grid[2][0])
+        if (grid[0][0] == 'X')
+            x++;
+        else if (grid[0][0] == 'O')
+            o++;
+    if (grid[0][1] == grid[1][1] && grid[0][1] == grid[2][1])
+        if (grid[0][1] == 'X')
+            x++;
+        else if (grid[0][1] == 'O')
+            o++;
+    if (grid[0][2] == grid[1][2] && grid[0][2] == grid[2][2])
+        if (grid[0][2] == 'X')
+            x++;
+        else if (grid[0][2] == 'O')
+            o++;
+    if (grid[0][0] == grid[0][1] && grid[0][0] == grid[0][2])
+        if (grid[0][0] == 'X')
+            x++;
+        else if (grid[0][0] == 'O')
+            o++;
+    if (grid[1][0] == grid[1][1] && grid[1][0] == grid[1][2])
+        if (grid[1][0] == 'X')
+            x++;
+        else if (grid[1][0] == 'O')
+            o++;
+    if (grid[2][0] == grid[2][1] && grid[2][0] == grid[2][2])
+        if (grid[2][0] == 'X')
+            x++;
+        else if (grid[2][0] == 'O')
+            o++;
+    if (grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2])
+        if (grid[0][0] == 'X')
+            x++;
+        else if (grid[0][0] == 'O')
+            o++;
+    if (grid[2][0] == grid[1][1] && grid[2][0] == grid[0][2])
+        if (grid[2][0] == 'X')
+            x++;
+        else if (grid[2][0] == 'O')
+            o++;
+    if (x * o > 0)
         return 0;
 
+    x = o = 0;
     for (i = 0; i < 3; i++)
         for (j = 0; j < 3; j++)
             if (grid[i][j] == 'X')
